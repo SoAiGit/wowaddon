11f62bcc85cd71c472a40b48741bd45846d54a04
soai <soai2018@163.com>
2019-07-09 07:12:49 +0800

    完成了

--- soai.lua
+++ soai.lua
@@ -1,50 +1,58 @@
 SlashCmdList['RELOAD'] = function() ReloadUI() end 
 SLASH_RELOAD1 = '/rl'
 
-soai = {}
+local soai = CreateFrame("Frame")
 _G["soai"] = soai
-
-if soaiDB == nil then
-	soaiDB = {}
-end
-
-local realm = GetRealmName()
-local faction = UnitFactionGroup("player")
-local playername = UnitName("player")
-local level = UnitLevel("player")
-local _, class = UnitClass("player")
-
-if not realm or not faction or not playername or not level or not class then return end
-
-soaiDB[playername .. "-" .. realm] = {}
-temp = soaiDB[playername .. "-" .. realm]
-temp.playername = playername
-temp.realm = realm
-temp.faction = faction
-temp.level = level
-temp.class = class
-
-for k, v in pairs(soaiDB) do
-    print(k, v)
-end
-
--- SendMailNameEditBox:SetText(XXXXX)
--- SendMailNameEditBox:HighlightText()
-
-local alts = CreateFrame("Frame", "F_alts", MailFrame)
-alts:SetWidth(250)
-alts:SetHeight(MailFrame:GetHeight())
-alts:SetPoint("TOPLEFT", "MailFrame", "TOPRIGHT")
-alts:SetBackdrop({
-			bgFile = [[Interface\DialogFrame\UI-DialogBox-Background]],
-		})
-alts:SetBackdropColor(0,0,0,1)
+soai:RegisterEvent("ADDON_LOADED")
 
 
-local b1 = CreateFrame("Button", nil , alts, "StaticPopupButtonTemplate");
-b1:SetSize(246, 30);
-b1:SetPoint("TOPLEFT","F_alts","TOPLEFT",2,0);
-b1:SetText(playername .. "-" .. realm);
-b1:SetScript("OnClick", function()
-	SendMailNameEditBox:SetText(b1:GetText())
-	end)
+function soai:OnEvent(event, arg1)
+	if event == "ADDON_LOADED" and arg1 == "soai" then
+		print ("soai addon load")
+		if soaiDB  == nil then
+			soaiDB = {}
+			print ("找不到配置文件")
+		end	
+		local realm = GetRealmName()
+		local faction = UnitFactionGroup("player")
+		local playername = UnitName("player")
+		local level = UnitLevel("player")
+		local _, class = UnitClass("player")
+
+		local btn_high = 60
+		local btn_count = 0
+
+		if not realm or not faction or not playername or not level or not class then return end
+
+		soaiDB[playername .. "-" .. realm] = {}
+		local temp = soaiDB[playername .. "-" .. realm]
+		temp.playername = playername
+		temp.realm = realm
+		temp.faction = faction
+		temp.level = level
+		temp.class = class
+
+		local alts = CreateFrame("Frame", "F_alts", MailFrame)
+		alts:SetWidth(250)
+		alts:SetPoint("TOPLEFT", "MailFrame", "TOPRIGHT")
+		alts:SetBackdrop({
+					bgFile = [[Interface\DialogFrame\UI-DialogBox-Background]],
+				})
+		alts:SetBackdropColor(0,0,0,1)
+
+		for k, v in pairs(soaiDB) do
+			local b1 = CreateFrame("Button", nil , alts, "StaticPopupButtonTemplate");
+			b1:SetSize(246, btn_high);
+			b1:SetPoint("TOPLEFT","F_alts","TOPLEFT",2,-btn_high * btn_count);
+			btn_count = btn_count + 1
+			b1:SetText(k .. "\n" .. "LV." .. v.level .. "   " .. v.class);
+			b1:SetScript("OnClick", function()
+				SendMailNameEditBox:SetText(k)
+				SendMailNameEditBox:HighlightText()
+				end)
+		end
+		alts:SetHeight(btn_high * btn_count)
+	end
+end	
+
+soai:SetScript("OnEvent", soai.OnEvent);
