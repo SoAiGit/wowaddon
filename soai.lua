1939898961b253c612157aa3499811618923306e
soai <soai2018@163.com>
2019-07-10 01:04:34 +0800

    以OO方式 重写 ， 使用了自定义按钮式样

--- soai.lua
+++ soai.lua
@@ -5,6 +5,13 @@ local soai = CreateFrame("Frame")
 _G["soai"] = soai
 soai:RegisterEvent("ADDON_LOADED")
 
+--[[ 
+OO结构： 
+	插件主体： soai, 已经注册到全局变量中
+	主Frame： soai.alts , 挂载在 MailFrame 上, 共享父窗体的状态 比如显示和隐藏 可以少写点代码
+	按钮： soai.alts.btn0, 前缀固定为btn, 数字从0开始顺下去, btn1, btn2, btn3 ...
+	文本： soai.alts.btn0.str, 系统默认使用 FontString 这个类, 我懒得去看基类的代码 就这样吧
+]]--
 
 function soai:OnEvent(event, arg1)
 	if event == "ADDON_LOADED" and arg1 == "soai" then
@@ -33,26 +40,28 @@ function soai:OnEvent(event, arg1)
 		temp.level = level
 		temp.class = class
 
-		local alts = CreateFrame("Frame", "F_alts", MailFrame)
-		alts:SetWidth(btn_width)
-		alts:SetPoint("TOPLEFT", "MailFrame", "TOPRIGHT")
-		alts:SetBackdrop({
+
+		soai.alts = CreateFrame("Frame", nil , MailFrame)
+		soai.alts:SetWidth(btn_width)
+		soai.alts:SetPoint("TOPLEFT", "MailFrame", "TOPRIGHT")
+		soai.alts:SetBackdrop({
 					bgFile = "Interface\\Buttons\\WHITE8x8",
 				})
-		alts:SetBackdropColor(0,0,0,0.8)
+		soai.alts:SetBackdropColor(0,0,0,0.8)
+
 
 		for k, v in pairs(soaiDB) do
-			local b1 = CreateFrame("Frame", "abtn"..btn_count, alts);
+			local b1 = CreateFrame("Frame", nil, soai.alts);
 			b1:SetSize(btn_width, btn_high);
-			b1:SetPoint("TOPLEFT","F_alts","TOPLEFT",0,-btn_high * btn_count);
+			b1:SetPoint("TOPLEFT",soai.alts ,"TOPLEFT",0,-btn_high * btn_count);
 
-			local str = b1:CreateFontString("astr"..btn_count, "ARTWORK")
-			str:SetSize(btn_width-10, btn_high)
-			str:SetPoint("TOPLEFT", "abtn"..btn_count,5,0)
-			str:SetFont("fonts\\ARHei.ttf", 14)
-			str:SetJustifyH("LEFT")
-			str:SetTextColor(0,1,1)
-			str:SetText(k .. " LV." .. v.level .. " " .. v.class)
+			b1.str = b1:CreateFontString(nil, "ARTWORK")
+			b1.str:SetSize(btn_width-10, btn_high)
+			b1.str:SetPoint("TOPLEFT", b1, 5,0)
+			b1.str:SetFont("fonts\\ARHei.ttf", 14)
+			b1.str:SetJustifyH("LEFT")
+			b1.str:SetTextColor(0,1,1)
+			b1.str:SetText(k .. " LV." .. v.level .. " " .. v.class)
 
 			b1:SetScript("OnMouseUp", function(f)
 				SendMailNameEditBox:SetText(k)
@@ -65,10 +74,13 @@ function soai:OnEvent(event, arg1)
 			b1:SetScript("OnLeave", function(f)
 				f:SetBackdrop(nil)
 				end)	
+
+			soai.alts["btn" .. btn_count] = b1
+
 			btn_count = btn_count + 1
 		end
 
-		alts:SetHeight(btn_high * btn_count)
+		soai.alts:SetHeight(btn_high * btn_count)
 	end
 end	
 
